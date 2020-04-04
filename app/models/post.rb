fac8f5de805494c02c6a1b8555e2b127faa16b84
Varras <cassmcd41@gmail.com>
2020-04-04 15:58:33 -0700

    included post and user associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :user
+    validates :title, length: { maximum: 50 }, presence: true
+    validates :body, length: { maximum: 255 }, presence: true
+    validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+    validates :name, length: { maximum: 30 }, presence: true
+    validates :email, length: { maximum: 255 }, presence: true
+    validates :password, length: { minimum: 6 }, presence: true
 end
